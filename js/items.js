55ea4e039a5273e9e35852e8e8065777a0498355
emerzon1 <59055006+emerzon1@users.noreply.github.com>
2020-04-24 12:00:23 -0400

    Create items.js

---
+++ js/items.js
@@ -0,0 +1,28 @@
+var compost = [
+	"Banana Peel",
+	"Fruit",
+	"Pasta noodles",
+	"Rice",
+	"Meat",
+	"Cheese",
+	"Vegetables"
+]
+
+var garbage = [
+	"Fruit stickers",
+	"Ziploc bags",
+	"Foam containers",
+	"Paint cans",
+	"Oil & grease"
+]
+
+var recycle = [
+	"Aluminum foil",
+	"Glass bottles & jars",
+	"Cans",
+	"Paper",
+	"Milk cartons",
+	"Cardboard boxes",
+	"Plastic grocery bags",
+	"Plastic food containers (clean)"
+]
